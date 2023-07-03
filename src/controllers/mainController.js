b9da408c544e843c1fb07ba96a01762d390213f0
Roni Graizman <ronigraiz@gmail.com>
2023-07-03 14:08:57 -0300

    processregister

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,4 +1,6 @@
+const bcrypt = require('bcryptjs');
 
+const { User } = require("../database/models");
 
 module.exports = {
   home: async (req, res) => {    
@@ -6,10 +8,26 @@ module.exports = {
 
     res.render("home")
   },
+
   register: async (req, res) => {    
     
     
     res.render("register")
   },
+
+  processRegister: async (req, res) => {
+    const fullname = await req.body.fullname
+    const email = await req.body.email
+    const password = await bcrypt.hash(req.body.password, 12)
+    const profileImage = await req.body.file
+
+    const UserToCreate = await User.create({
+      fullname: fullname,
+      email: email,
+      password: password
+    })
+
+    res.render("login")
+  }
 
 };
--- src/routers/main-router.js
+++ src/routers/main-router.js
@@ -3,9 +3,10 @@ const router = Router();
 module.exports = router;
 
 const mainController = require("../controllers/mainController");
-const multerMiddleware = require("../middlewares/multerMiddleware");
+const multerMiddleware = require("../middlewares/multerMiddleware.js");
 
 
 router.get("/", mainController.home);
 router.get("/register", mainController.register);
+router.post("/register", multerMiddleware.single("image"), mainController.processRegister)
 
